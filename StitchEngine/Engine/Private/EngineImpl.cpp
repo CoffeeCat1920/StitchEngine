6ddaa9d6172229fc0715896858e660f919026551
CoffeeCat1920 <h0neycatcoffeego@proton.me>
2026-04-25 13:51:33 +0500

    Getting Registry to work

--- StitchEngine/Engine/Private/EngineImpl.cpp
+++ StitchEngine/Engine/Private/EngineImpl.cpp
@@ -2,23 +2,21 @@
 #include "ECS.hpp"
 #include "Engine.hpp"
 #include "RenderHandler.hpp"
+#include "SystemRegistry.hpp"
 #include "TextureHandler.hpp"
 #include "WindowHandler.hpp"
 
 #include <memory>
-
-struct CGravity {
-  float force = 0;
-  COMPONENT(CGravity, force)
-};
-REFLECTION(CGravity);
 
 class EnineImpl : public Engine {
 private:
   WindowHandler &g_windowHandler;
   RenderHandler &g_renderHandler;
   TextureHandler &g_textureHandler;
+
   ComponentRegistry &g_componentRegistry;
+  SystemRegistry &g_systemRegistry;
+
   ECS &g_ECS;
 
 public:
@@ -26,16 +24,18 @@ public:
       : g_windowHandler(GetWindowHandler(windowCfg)),
         g_renderHandler(GetRenderHandler()),
         g_textureHandler(GetTextureHandler()), g_ECS(ECS::Instance()),
-        g_componentRegistry(ComponentRegistry::Instance()) {}
+        g_componentRegistry(ComponentRegistry::Instance()),
+        g_systemRegistry(SystemRegistry::Instance()) {}
 
   void Init() override { g_windowHandler.Init(); }
 
   void Run() override {
     while (g_windowHandler.IsRunning()) {
       g_windowHandler.BeginFrame();
+      g_systemRegistry.RenderUpdate();
       g_renderHandler.RenderQueue();
-
       g_windowHandler.EndFrame();
+      g_systemRegistry.PhysicsUpdate();
     }
     g_windowHandler.Close();
   }
