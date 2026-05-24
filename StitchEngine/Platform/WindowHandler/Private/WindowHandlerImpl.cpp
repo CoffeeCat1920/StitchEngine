096758b564bde2597b39bbf64bd311a836059ab1
CoffeeCat1920 <h0neycatcoffeego@proton.me>
2026-05-25 01:01:13 +0500

    I wish I had read that book before interview

--- StitchEngine/Platform/WindowHandler/Private/WindowHandlerImpl.cpp
+++ StitchEngine/Platform/WindowHandler/Private/WindowHandlerImpl.cpp
@@ -1,30 +1,52 @@
 #include "WindowHandler.hpp"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_video.h>
+#include <iostream>
 
 class WindowHandlerImpl : public WindowHandler {
 private:
-public:
-  WindowHandlerImpl(WindowConfig cfg) : WindowHandler(cfg) {}
+  SDL_Window *gSDL_Window;
+  SDL_Renderer *gSDL_Renderer;
+  SDL_Event gSDL_Event;
 
-  ~WindowHandlerImpl() {}
+  bool running;
+
+public:
+  WindowHandlerImpl(WindowConfig cfg) : WindowHandler(cfg), running(true) {}
+
+  ~WindowHandlerImpl() {
+    SDL_DestroyWindow(gSDL_Window);
+    SDL_DestroyRenderer(gSDL_Renderer);
+  }
 
   void Init() override {
     WindowConfig cfg = WindowHandler::GetConfig();
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_UNFOCUSED |
-                   FLAG_BORDERLESS_WINDOWED_MODE);
-    InitWindow(cfg.w, cfg.h, cfg.title.c_str());
-    SetTargetFPS(60);
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+      std::cerr << SDL_GetError() << "\n";
+      return;
+    }
+    gSDL_Window = SDL_CreateWindow(cfg.title.c_str(), cfg.w, cfg.h, 0);
+    gSDL_Renderer = SDL_CreateRenderer(gSDL_Window, nullptr);
   }
 
   void BeginFrame() override {
-    BeginDrawing();
-    ClearBackground(GRAY);
+    while (SDL_PollEvent(&gSDL_Event)) {
+      if (gSDL_Event.type == SDL_EVENT_QUIT) {
+        running = false;
+      }
+    }
+    SDL_SetRenderDrawColor(gSDL_Renderer, 30, 60, 120, 255);
+    SDL_RenderClear(gSDL_Renderer);
   }
 
-  void EndFrame() override { EndDrawing(); }
+  void EndFrame() override { SDL_RenderPresent(gSDL_Renderer); }
 
-  void Close() override { CloseWindow(); }
+  void Close() override { running = false; }
 
-  bool IsRunning() override { return !WindowShouldClose(); }
+  bool IsRunning() override { return running; }
 };
 
 WindowHandler &GetWindowHandler(WindowConfig windowConfig) {
